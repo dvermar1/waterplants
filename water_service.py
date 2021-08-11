e328e35dfe7b039a7d81820c2a5a0f79180f78c5
Parag Taneja <tanejaparag@gmail.com>
2021-08-10 20:31:26 -0400

    Okta Verification

--- water_service.py
+++ water_service.py
@@ -4,19 +4,27 @@ import time
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
-
+import okta_jwt_verifier
 
 app = flask.Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
 
+jwt_verifier = JWTVerifier(issuer="https://dev-94896909.okta.com/oauth2/default",
+                           client_id="0oa1ebw5njlc9Tgnz5d7",
+                           audience='api://default')
 
 @app.route('/', methods=['GET'])
 def home():
+    if not is_authorized(request):
+        return "Unauthorized", 401
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 @app.route('/water', methods=['POST'])
 def water_plants():
+    if not is_authorized(request):
+        return "Unauthorized", 401
+
     var1 = request.args.get('key1')
     var2 = request.args.get('key2')
 
@@ -36,6 +44,14 @@ def pump_on(pump_pin = 7):
     GPIO.output(pump_pin, GPIO.LOW)
     time.sleep(10)
     GPIO.output(pump_pin, GPIO.HIGH)
+
+def is_authorized(request):
+    """Get access token from authorization header."""
+    try:
+        token = request.headers.get("Authorization").split("Bearer ")[1]
+        return is_access_token_valid(token, config["issuer"], config["client_id"])
+    except Exception:
+        return False
 
 if __name__ == "__main__":
     app.run()
